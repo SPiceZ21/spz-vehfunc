c9a3c54552ad42ad004fcf171388eb8062a5063f
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-06-29 13:49:54 +0530

    feat: add ox_lib dependency and script integration to fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,4 +4,8 @@ game 'gta5'
 description 'SPiceZ Vehicle Functions - Indicators, Hazards, Flash Headlights'
 version '1.0.1'
 
+shared_script '@ox_lib/init.lua'
+
 client_script 'client/main.lua'
+
+dependency 'ox_lib'
